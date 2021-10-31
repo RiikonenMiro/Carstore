9bee49fb27b78b442bc98710f642bf9353849755
Miro Riikonen <miro.riikonen@gmail.com>
2021-10-31 12:39:28 +0200

    First commit

---
+++ src/main/java/hh/swd0/Carstore/CarstoreApplication.java
@@ -0,0 +1,13 @@
+package hh.swd0.Carstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hh/swd0/Carstore/CarstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd0.Carstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
